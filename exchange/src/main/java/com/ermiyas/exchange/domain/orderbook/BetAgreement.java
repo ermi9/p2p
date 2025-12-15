17745f758f9fdab08b17dd3ebb02e56cb4d33e25
Ermiyas A <dgnrys03b15z315a@studenti.unime.it>
2025-12-15 12:38:30 +0100

    added orderbook files and tweaked some details on the orderbook logic

---
+++ exchange/src/main/java/com/ermiyas/exchange/domain/orderbook/BetAgreement.java
@@ -0,0 +1,25 @@
+package com.ermiyas.exchange.domain.orderbook;
+import com.ermiyas.exchange.common.Money;
+import com.ermiyas.exchange.common.Odds;
+import com.ermiyas.exchange.domain.offer.Position;
+
+public final class BetAgreement{
+    private final long offerId;
+    private final long makerUserId;
+    private final long takerUserId;
+    private final long outcomeId;
+
+    private final Position position;
+    private final Odds odds;
+    private final Money matchedAmount;
+    
+public BetAgreement(long offerId,long makerUserId,long takerUserId,long outcomeId,Position position,Odds odds, Money matchedAmount){
+    this.offerId=offerId;
+    this.makerUserId=makerUserId;
+    this.takerUserId=takerUserId;
+    this.outcomeId=outcomeId;
+    this.position=position;
+    this.odds=odds;
+    this.matchedAmount=matchedAmount;
+}
+}

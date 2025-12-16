15822dfcab00ead8d6ebe6bd8f417b3030634a8c
Ermiyas A <dgnrys03b15z315a@studenti.unime.it>
2025-12-16 11:29:52 +0100

    added Wallet and strict invariants

---
+++ exchange/src/main/java/com/ermiyas/exchange/domain/wallet/Wallet.java
@@ -0,0 +1,75 @@
+package com.ermiyas.exchange.domain.wallet;
+import com.ermiyas.exchange.common.Money;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+/*
+wallet inforces invariants like a balance can't be NEGATIVE
+*/
+public final class Wallet {
+    private final long userId;
+    private Money balance;
+    private final List<WalletTransaction> transactions;
+    public Wallet(long userId,Money startingBalance){
+        this.userId=userId;
+        this.balance=Objects.requireNonNull(startingBalance,"startingaBalance");
+        this.transactions=new ArrayList<>();
+    }
+    public long userId(){
+        return userId;
+    }
+    public Money balance(){
+        return balance;
+    }
+    public List<WalletTransaction> transactions(){
+        return Collections.unmodifiableList(transactions);
+    }
+    public WalletTransaction deposit(Money amount,String reference){
+        requirePositive(amount);
+        ensureSufficientFunds(amount);
+        balance=balance.plus(amount);
+    WalletTransaction tx=WalletTransaction.of(userId,WalletTransactionType.DEPOSIT,amount,reference);
+    transactions.add(tx);
+    return tx;
+    }
+    public WalletTransaction withdraw(Money amount, String reference){
+        requirePositive(amount);
+        ensureSufficientFunds(amount);
+        balance=balance.minus(amount);
+         WalletTransaction tx=WalletTransaction.of(userId,WalletTransactionType.DEPOSIT,amount,reference);
+    transactions.add(tx);
+    return tx;
+
+    }
+
+    public WalletTransaction debitForBet(Money amount, String reference){
+        requirePositive(amount);
+        ensureSufficientFunds(amount);
+        balance=balance.minus(amount);
+        WalletTransaction tx=WalletTransaction.of(userId,WalletTransactionType.DEPOSIT,amount,reference);
+    transactions.add(tx);
+    return tx;
+
+    }
+    public WalletTransaction creditForBet(Money amount, String reference){
+        requirePositive(amount);
+        balance=balance.plus(amount);
+        WalletTransaction tx=WalletTransaction.of(userId,WalletTransactionType.DEPOSIT,amount,reference);
+    transactions.add(tx);
+    return tx;
+    }
+    private void ensureSufficientFunds(Money amount){
+        //compare values
+        if(balance.value().compareTo(amount.value())<0)
+            throw new InsufficientFundsException(balance, amount);
+
+    }
+    private void requirePositive(Money amount){
+        Objects.requireNonNull(amount, "amount");
+        if(amount.value().signum()<0)
+            throw new IllegalArgumentException("Amount must be positive!");
+
+    }
+
+}

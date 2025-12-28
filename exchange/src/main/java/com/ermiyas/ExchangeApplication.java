021ee562fae46fdb96908230d9bc16bca17889fd
Ermiyas A <dgnrys03b15z315a@studenti.unime.it>
2025-12-28 18:01:59 +0100

    Enable scheduling for ExchangeApplication

--- exchange/src/main/java/com/ermiyas/ExchangeApplication.java
+++ exchange/src/main/java/com/ermiyas/ExchangeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling; // 1. Add thi
 
 @SpringBootApplication
 @EnableCaching
-@EnableScheduling // 2. Add this line to turn on your Crawler
+@EnableScheduling 
 public class ExchangeApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExchangeApplication.class, args);
